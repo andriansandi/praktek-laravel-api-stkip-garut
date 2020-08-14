17af7d961b60ae74d67266368517ec86b632eca5
Sandi Andrian <andrian.sandi@gmail.com>
2020-08-15 00:18:27 +0700

    Penambahan model untuk table Registration

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Registration Controller
+ * 
+ * @author  Sandi Andrian <andrian.sandi@gmail.com>
+ * @since   Aug 15, 2020
+ **/
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create() 
+    {
+
+    }
+}
---
+++ app/Registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Registration Model
+ *
+ * @author  Sandi Andrian <andrian.sandi@gmail.com>
+ * @since   Aug 15, 2020 
+ */
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Registration extends Model
+{
+    protected $table = 'registration';
+}
